0be4d10a07b0ee030be44929256ef18064892c14
KirillDanProg <kirill2898v@gmail.com>
2022-11-12 20:36:13 +0300

    updateId for fetching user messages

--- src/components/Dialogs/Dialog.tsx
+++ src/components/Dialogs/Dialog.tsx
@@ -1,21 +1,26 @@
-import React, {FC} from 'react';
+import React, {FC, memo} from 'react';
 import {DialogType} from "../../redux/dialogsReducer/dialogs-reducer";
 import defaultAvatar from "../../assets/user.png"
 import {Avatar} from "../Avatar";
+import styles from "./Dialogs.module.css"
 
 type DialogPropsType = {
     dialogData: DialogType
+    changeId: (id: number) => void
 }
-export const Dialog: FC<DialogPropsType> = (props) => {
+export const Dialog: FC<DialogPropsType> = memo((props) => {
     const {dialogData} = props
     const avatar = dialogData.photos.small || defaultAvatar
 
+    const changeIdHandler = () => {
+        props.changeId(dialogData.id)
+    }
+
     return (
-        <div>
+        <div className={styles.dialogBox} onClick={changeIdHandler}>
             <Avatar src={avatar}/>
             <div>{dialogData.userName}</div>
-
         </div>
     );
-};
+});
 
