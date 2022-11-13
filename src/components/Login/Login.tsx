e9cdb7022afd20804951a721eb4dafe84d73c82e
KirillDanProg <kirill2898v@gmail.com>
2022-11-13 19:59:46 +0300

    custom text field added/login page updated

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,48 +1,54 @@
 import React, {FC} from 'react';
 import {SubmitHandler, useForm} from "react-hook-form";
+import {yupResolver} from '@hookform/resolvers/yup';
+import * as yup from "yup";
 import styles from "./Login.module.css"
+import {Button} from "../../common/superComponents/Button";
+import {PositionedComponent} from "../../common/superComponents/PositionComponent";
+import {TextField} from "../../common/superComponents/TextField";
 
 type LoginPropsType = {
     login: string | null
     authorization: (data: LoginDataType) => void
 }
-const Login: FC<LoginPropsType> = (props) => {
-    return (
-        <div className={styles.loginContainer}>
-            <LoginForm {...props}/>
-        </div>
-    );
-};
-
-
 export type LoginDataType = {
-    login: string,
+    email: string,
     password: string,
     rememberMe: boolean
 };
+const schema = yup.object({
+    email: yup.string().email().required("Email is required"),
+    password: yup
+        .string()
+        .required("Password is required")
+        .min(6, "Password is too short - should be 6 chars minimum"),
+}).required();
 
-export const LoginForm: FC<LoginPropsType> = (props) => {
+export const Login: FC<LoginPropsType> = (props) => {
+    const inputRef = React.createRef()
     const onSubmit: SubmitHandler<LoginDataType> = data => {
         props.authorization(data)
     }
-    const {register, handleSubmit, formState: {errors}} = useForm<LoginDataType>();
+    const {register, handleSubmit, formState: {errors}} = useForm<LoginDataType>({
+        resolver: yupResolver(schema)
+    });
 
     return (
-        <>
+        <PositionedComponent center className={styles.loginContainer}>
             {
                 <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-                    <input type={"text"} {...register("login", {required: true})}  />
+                    <TextField innerRef={inputRef}
+                               {...register("email",
+                                   {required: true, pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g})}  />
 
-                    <input type={"password"} {...register("password", {required: true})} />
+                    <TextField type={"password"}  {...register("password",
+                        {required: true, minLength: 4})} />
 
-                    {errors.password || errors.login && <span>This field is required</span>}
+                    {errors.password || errors.email && <span>This field is required</span>}
 
-                    <input type="submit"/>
+                    <Button type="submit">Login</Button>
                 </form>
             }
-        </>
+        </PositionedComponent>)
+};
 
-    )
-}
-
-export default Login;
