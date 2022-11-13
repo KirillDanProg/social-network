ccddf61c70ed9633f4bbe3958165ad22370a257e
KirillDanProg <kirill2898v@gmail.com>
2022-11-13 19:51:15 +0300

    Theme provider added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, {useEffect} from 'react';
 import './App.css';
-
 import {BrowserRouter, Route, Routes,} from "react-router-dom";
 import {Sidebar} from "./components/Sidebar/Sidebar";
 import {Friends} from "./components/Friends/Friends";
@@ -12,14 +11,15 @@ import {useAppDispatch, useAppSelector} from "./common/hooks";
 import Loader from "./common/Loader/Loader";
 import {appInit} from "./redux/appReducer/app-reducer";
 import {Dialogs} from "./components/Dialogs/Dialogs";
-import {getFriendsTC} from "./redux/usersReducer/users-reducer";
-
+import {ThemeProvider} from "styled-components";
+import {themes} from "./theme/themes";
+import {StyledAppContainer} from "./common/superComponents/StyledApp";
+import {StyledMainContainer} from "./common/superComponents/StyledMain";
 
 const App = () => {
     const isAppInit = useAppSelector(state => state.application.isInit)
+    const theme = useAppSelector(state => state.application.theme)
     const dispatch = useAppDispatch()
-
-
 
     useEffect(() => {
         dispatch(appInit())
@@ -29,21 +29,26 @@ const App = () => {
         :
         (
             <BrowserRouter>
-                <div className="App">
-                    <HeaderContainer/>
-                    <Sidebar/>
-                    <div className="AppContent">
-                        <Routes>
-                            <Route path="/profile" element={<Profile/>}>
-                                <Route path={":userId"}/>
-                            </Route>
-                            <Route path="/dialogs" element={<Dialogs/>}/>
-                            <Route path="/users" element={<UsersContainer/>}/>
-                            <Route path="/friends" element={<Friends/>}/>
-                            <Route path="/login" element={<LoginContainer/>}/>
-                        </Routes>
-                    </div>
-                </div>
+
+                <ThemeProvider theme={themes[theme]}>
+
+                    <StyledAppContainer className="App">
+                        <HeaderContainer/>
+                        <Sidebar/>
+                        <StyledMainContainer className="AppContent">
+                            <Routes>
+                                <Route path="/profile" element={<Profile/>}>
+                                    <Route path={":userId"}/>
+                                </Route>
+                                <Route path="/dialogs" element={<Dialogs/>}/>
+                                <Route path="/users" element={<UsersContainer/>}/>
+                                <Route path="/friends" element={<Friends/>}/>
+                                <Route path="/login" element={<LoginContainer/>}/>
+                            </Routes>
+                        </StyledMainContainer>
+                    </StyledAppContainer>
+
+                </ThemeProvider>
             </BrowserRouter>
         );
 }
