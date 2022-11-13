08b419b4b648fbd0767337c0b4692b519f0158a6
KirillDanProg <kirill2898v@gmail.com>
2022-11-13 20:02:20 +0300

    themes for app created

---
+++ src/theme/themes.ts
@@ -0,0 +1,18 @@
+
+const darkTheme = {
+    backgroundColor: "rgb(20 20 20)",
+    color: "rgb(255, 255, 255)",
+    primary: "#222222",
+}
+const lightTheme = {
+    backgroundColor: "rgb(255 255 255)",
+    color: "#0a7ea4",
+    primary: "#0a7ea4",
+    secondary: "#eaecee"
+}
+export const themes = {
+    "dark": darkTheme,
+    "light": lightTheme
+}
+
+
