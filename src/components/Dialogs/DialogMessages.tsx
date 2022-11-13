9db3760d95543f21e2df4990858368c08fb98604
KirillDanProg <kirill2898v@gmail.com>
2022-11-13 20:04:12 +0300

    dialog messages added

--- src/components/Dialogs/DialogMessages.tsx
+++ src/components/Dialogs/DialogMessages.tsx
@@ -1,21 +1,46 @@
 import React, {useEffect} from 'react';
 import {useAppDispatch, useAppSelector} from "../../utils/hooks/reduxHooks";
-import {fetchMessagesTC} from "../../redux/dialogsReducer/dialogs-reducer";
+import {deleteUserMessageTC, DialogType, fetchMessagesTC} from "../../redux/dialogsReducer/dialogs-reducer";
+import {DialogMessage} from "./Message/DialogMessage";
+import {Avatar} from "../../common/superComponents/Avatar";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {MessageContainer} from "../../common/superComponents/MessageContainer";
+import styles from "./Dialogs.module.css"
 
-export const DialogMessages = (props: { id: number | null }) => {
+
+export const DialogMessages = (props: { data: DialogType }) => {
 
     const dispatch = useAppDispatch()
-    const messages = useAppSelector(state => state.dialogs.messagesData[props.id + ""])
+    const sandedMessages = useAppSelector(state => state.dialogs.messagesData[props.data.id + ""])
+    const profileMePhoto = useAppSelector(state => state.profile.profileData.photos.small)
+
+    const deleteMessageHandler = (id: string) => {
+        dispatch(deleteUserMessageTC(props.data.id, id))
+    }
 
     useEffect(() => {
-        if (props.id) {
-            dispatch(fetchMessagesTC(props.id))
+        if (props.data.id) {
+            dispatch(fetchMessagesTC(props.data.id))
         }
-    }, [props.id])
+    }, [props.data.id])
 
     return (
         <div>
-            {messages?.map(m => <div key={m.id}>{m.body}</div>)}
+            {sandedMessages?.map(m => {
+                return (
+                    <MessageContainer key={m.id}>
+                        <FontAwesomeIcon icon={["fas", "trash"]}
+                                         className={styles.icon}
+                                         onClick={() => deleteMessageHandler(m.id)}
+                        />
+                        <Avatar src={profileMePhoto} width={"50px"}/>
+                        <DialogMessage>
+                            <div style={{fontSize: "14px"}}> {m.body}</div>
+                        </DialogMessage>
+                    </MessageContainer>
+
+                )
+            })}
         </div>
     );
 };
