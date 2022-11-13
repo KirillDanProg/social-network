092b031fd9c9251d509a8bff079b14ee37d3df47
KirillDanProg <kirill2898v@gmail.com>
2022-11-13 23:29:42 +0300

    refactoring

--- src/common/superComponents/StyledApp.tsx
+++ src/common/superComponents/StyledApp.tsx
@@ -4,7 +4,8 @@ import styled from "styled-components";
 
 const AppContainer = styled.div`
   background: ${props => props.theme.backgroundColor};
-  color: ${props => props.theme.color}
+  color: ${props => props.theme.color};
+  transition: all 0.5s ease;
 `
 
 export const StyledAppContainer = (props) => {
