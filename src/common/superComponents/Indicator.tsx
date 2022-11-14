49635b26c52e4b2e2fa1aee308c3739f95b12728
KirillDanProg <kirill2898v@gmail.com>
2022-11-14 10:18:34 +0300

    indicator for message status added

--- src/common/superComponents/Indicator.tsx
+++ src/common/superComponents/Indicator.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import styled from "styled-components";
 
 const StyledIndicator = styled.span`
-display: inline-block;
+  display: inline-block;
   width: 5px;
   height: 5px;
+  align-self: center;
   border-radius: 50%;
-  box-shadow: 0 0 2px ;
+  box-shadow: 0 0 2px;
+  background-color: #0babff;
 `
 export const Indicator = () => {
     return (
