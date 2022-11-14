b745d9f6c98f87b6d5d2cc8fa599de5373dd838a
KirillDanProg <kirill2898v@gmail.com>
2022-11-14 10:21:59 +0300

    redux devtools added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,4 @@
-import {AnyAction, applyMiddleware, combineReducers, createStore} from "redux";
+import {AnyAction, applyMiddleware, combineReducers, compose} from "redux";
 import profileReducer, {ProfileActionsType} from "./profileReducer/profile-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import {UsersActionsType, usersReducer} from "./usersReducer/users-reducer";
@@ -6,6 +6,7 @@ import {AuthActionsType, authReducer} from "./authReducer/authReducer";
 import thunkMiddleware, {ThunkAction, ThunkDispatch} from 'redux-thunk'
 import {appReducer} from "./appReducer/app-reducer";
 import {DialogsActionTypes, dialogsReducer} from "./dialogsReducer/dialogs-reducer";
+import { legacy_createStore as createStore} from 'redux'
 
 const rootReducer = combineReducers({
     dialogs: dialogsReducer,
@@ -21,14 +22,16 @@ export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = ThunkDispatch<RootState, unknown, AppActionsType>
 export type AppActionsType = UsersActionsType | DialogsActionTypes | AuthActionsType | ProfileActionsType
 
-export type AppThunk<ReturnType = any> = ThunkAction<
-
-    ReturnType,
+export type AppThunk<ReturnType = any> = ThunkAction<ReturnType,
     RootState,
     unknown,
-    AnyAction
-    >
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+    AnyAction>
+
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunkMiddleware),
+));
 // @ts-ignore
 window.store = store
 
