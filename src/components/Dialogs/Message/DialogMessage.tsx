11c8bea92b3da6e3f5ae00e22e1d0d1587907cae
KirillDanProg <kirill2898v@gmail.com>
2022-11-14 10:21:10 +0300

    styled dialog message added

--- src/components/Dialogs/Message/DialogMessage.tsx
+++ src/components/Dialogs/Message/DialogMessage.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "./DialogMessage.module.css";
+import styled from "styled-components";
 
-
+const StyledMessageContainer = styled.div`
+  background-color: ${props => props.theme.subBackground};
+  &::after {
+    background: radial-gradient(circle at 0 0, rgba(0, 0, 0, 0) 20px, ${props => props.theme.subBackground} 0);
+  }
+`
 export const DialogMessage = ({children}) => {
 
     return (
-        <div className={styles.messageContainer}>
+        <StyledMessageContainer className={styles.messageContainer}>
             {children}
-        </div>
+        </StyledMessageContainer>
     )
 }
