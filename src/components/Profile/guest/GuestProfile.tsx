9556f33bd39d0867c8c4adfeb285acef30106e6e
KirillDanProg <kirill2898v@gmail.com>
2022-11-15 19:30:17 +0300

    guest profile created

---
+++ src/components/Profile/guest/GuestProfile.tsx
@@ -0,0 +1,92 @@
+import React, {FC, useEffect} from "react";
+import styles from "../Profile.module.css"
+import EditableSpan from "../../../common/superComponents/EditableSpan";
+import {Avatar} from "../../../common/superComponents/Avatar";
+import defaultUserImg from "../../../assets/user.png"
+import {getProfileDataTC, updateUserPhotoTC} from "../../../redux/profileReducer/profile-reducer";
+import {useAppDispatch, useAppSelector} from "../../../utils/hooks/reduxHooks";
+import {ProfileDescription} from "./ProfileDescription";
+import {Button} from "../../../common/superComponents/Button";
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons/faEnvelope";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {Flex} from "../../../common/superComponents/Flex";
+import {useNavigate} from "react-router-dom";
+import {refreshDialogTC} from "../../../redux/dialogsReducer/dialogs-reducer";
+import {faUserGroup} from "@fortawesome/free-solid-svg-icons/faUserGroup";
+import {followTC} from "../../../redux/usersReducer/users-reducer";
+
+
+type ProfileInfoType = {
+    idFromURL: number
+}
+export const GuestProfile: FC<ProfileInfoType> = (props) => {
+    const dispatch = useAppDispatch()
+    const navigate = useNavigate()
+
+    let profileData = useAppSelector(state => state.profile.profileData)
+
+    const userPhoto = (profileData.photos && profileData.photos.large) || defaultUserImg
+
+    const goToDialogsHandler = () => {
+        navigate("/dialogs")
+        dispatch(refreshDialogTC(profileData.userId))
+    }
+
+    const uploadPhotoHandler = (file: File) => {
+        dispatch(updateUserPhotoTC(file))
+    }
+
+    const addToFriends = () => {
+        dispatch(followTC(profileData.userId))
+    }
+    useEffect(() => {
+        dispatch(getProfileDataTC(props.idFromURL))
+    }, [])
+
+    return (
+        <div className={styles.profileInfo}>
+            <Flex direction={"column"} gap={"10px"}>
+
+                <Avatar width={"200px"}
+                        profile={"true"}
+                        shape={"square"}
+                        src={userPhoto}
+                        uploadPhoto={uploadPhotoHandler}
+                />
+
+                <div className={styles.name}>{profileData.fullName}</div>
+                <Button onClick={addToFriends}>
+                    Add to friends
+                    <FontAwesomeIcon icon={faUserGroup}/>
+                </Button>
+
+                <Button onClick={goToDialogsHandler}>
+                    Send message
+                    <FontAwesomeIcon size={"lg"} icon={faEnvelope}/>
+                </Button>
+
+            </Flex>
+            <>
+                <UserStatus
+                    value={profileData.status}
+                />
+
+                <ProfileDescription/>
+            </>
+        </div>
+    )
+}
+
+
+type UserStatusType = {
+    value: string | null
+    callback?: (value: string) => void
+}
+export const UserStatus: FC<UserStatusType> = (props) => {
+    return (
+        <div className={styles.status}>
+            <EditableSpan value={props.value} callback={props.callback}/>
+        </div>
+    )
+}
+
