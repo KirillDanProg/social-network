0aae8e7ef31686454c04e11c453c10d60517941a
KirillDanProg <kirill2898v@gmail.com>
2022-11-17 09:23:18 +0300

    styled moved from module.css to styledComponent

--- src/common/superComponents/StyledApp.tsx
+++ src/common/superComponents/StyledApp.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import styled from "styled-components";
+import {device} from "../mediaqueries/media";
 
 
 const AppContainer = styled.div`
+  display: grid;
+  gap: 20px;
+  margin: 0 auto;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: 50px 1fr;
+  grid-column-gap: 5px;
+  grid-row-gap: 5px;
   background: ${props => props.theme.backgroundColor};
   color: ${props => props.theme.color};
   transition: all 0.5s ease;
+  @media ${device.mobileS} {
+    gap: 10px;
+    grid-column-gap: 4px;
+    grid-row-gap: 4px;
+  }
 `
 
 export const StyledAppContainer = (props) => {
