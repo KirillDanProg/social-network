07c9e94b2102e463dba94a6105e2e0456e6dd9fb
KirillDanProg <kirill2898v@gmail.com>
2022-11-20 20:41:46 +0300

    dialogs roles added sender/recipient

--- src/components/Dialogs/Message.tsx
+++ src/components/Dialogs/Message.tsx
@@ -12,22 +12,28 @@ import {faTrash} from "@fortawesome/free-solid-svg-icons/faTrash";
 type MessagePropsType = {
     messageData: MessageType
     deleteMessage: (id: string) => void
+    recipientPhoto: string
 }
 
-export const Message: FC<MessagePropsType> = ({messageData, deleteMessage}) => {
+export const Message: FC<MessagePropsType> = ({messageData, deleteMessage, recipientPhoto}) => {
 
+    const ownerId = useAppSelector(state => state.userAccess.personalData.userId)
+    //owner avatar
     const profileMePhoto = useAppSelector(state => state.userAccess.personalData.photos.small)
+    // if not owner photo than recipient photo
+    const avatar = messageData.senderId !== ownerId ? recipientPhoto : profileMePhoto
 
     const viewed = messageData.viewed
     return (
-        <MessageContainer key={messageData.id}>
+        <MessageContainer recipientMessage={messageData.senderId !== ownerId}
+                          key={messageData.id}>
             {!viewed && <Indicator/>}
             <FontAwesomeIcon icon={faTrash}
                              className={styles.icon}
                              onClick={() => deleteMessage(messageData.id)}
             />
-            <Avatar src={profileMePhoto} width={"50px"}/>
-            <DialogMessage>
+            <Avatar src={avatar} width={"50px"}/>
+            <DialogMessage recipientMessage={messageData.senderId !== ownerId}>
                 <div style={{fontSize: "14px"}}> {messageData.body}</div>
             </DialogMessage>
         </MessageContainer>
--- src/components/Dialogs/Message/DialogMessage.tsx
+++ src/components/Dialogs/Message/DialogMessage.tsx
@@ -1,18 +1,45 @@
 import React from "react";
-import styles from "./DialogMessage.module.css";
 import styled from "styled-components";
 
-const StyledMessageContainer = styled.div`
+const StyledMessage = styled.div<any>`
+  position: relative;
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: end;
+  flex-wrap: nowrap;
+  border-radius: 10px 10px 0 10px;
+  padding: 5px 10px;
+  align-items: center;
   background-color: ${props => props.theme.subBackground};
+
   &::after {
+    content: "";
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    bottom: 0;
+    right: -20px;
+    transform: rotate(90deg);
     background: radial-gradient(circle at 0 0, rgba(0, 0, 0, 0) 20px, ${props => props.theme.subBackground} 0);
   }
+
+  ${props => props.recipientMessage && `
+    flex-direction: row;
+    justify-content: start;
+    border-radius: 10px 10px 10px 0;
+    
+    &::after {
+    left: -20px;
+      transform: rotate(0);
+    background: radial-gradient(circle at 0 0, rgba(0, 0, 0, 0) 20px, ${props => props.theme.subBackground} 0);
+  }
+  `}
 `
-export const DialogMessage = ({children}) => {
+export const DialogMessage = ({children, ...props}) => {
 
     return (
-        <StyledMessageContainer className={styles.messageContainer}>
+        <StyledMessage {...props}>
             {children}
-        </StyledMessageContainer>
+        </StyledMessage>
     )
 }
